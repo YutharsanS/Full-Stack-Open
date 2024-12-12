bbdffc875200bb6c73b8c7ed77c8e9999f1f603d
YutharsanS <yutharsansivabalan@gmail.com>
2024-12-12 14:04:06 +0530

    finished part3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,6 @@ const Contact = require('./models/contact')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const { default: mongoose } = require('mongoose')
 const app = express()
 
 const PORT = process.env.PORT
@@ -13,106 +12,112 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-morgan.token('content', (request, response) => {
-    const body = request.body
-    return JSON.stringify(body)
+morgan.token('content', (request) => {
+  const body = request.body
+  return JSON.stringify(body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if (error.name === 'CastError') {
-        response.status(500).send({ messsage: error.message })
-    } else if (error.name === 'ValidationError') {
-        response.status(500).send({ message: error.message})
-    }
+  if (error.name === 'CastError') {
+    response.status(500).send({ messsage: error.message })
+  } else if (error.name === 'ValidationError') {
+    response.status(500).send({ message: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 
 app.get('/', (request, response) => {
-    response.send("<h1>Persons</h1>")
+  response.send('<h1>Persons</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(res => response.json(res))
+  Contact.find({}).then(res => response.json(res))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const contact = new Contact({
-        name: body.name,
-        number: body.number
-    })
+  const contact = new Contact({
+    name: body.name,
+    number: body.number
+  })
 
-    contact.save().then(
-        (savedNote) => {
-            response.json(savedNote)
-        }
-    ).catch(error => next(error))
+  contact.save().then(
+    (savedNote) => {
+      response.json(savedNote)
+    }
+  ).catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    const dateTime = new Date();
-    const result =
-    `
-            <p>Phonebook has info for ${persons.length} people</p>
+app.get('/info', (request, response, next) => {
+  const dateTime = new Date()
+
+  Contact.countDocuments({})
+    .then((count) => {
+      const result =
+            `
+            <p>Phonebook has info for ${count} people</p>
             <p>${dateTime.toString()}</p>
-    `
-    response.send(result)
+            `
+      response.send(result)
+    }).catch((err) => {
+      next(err)
+    })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Contact.findById(request.params.id)
-        .then(res => {
-            if (res) {
-                response.json(res)
-            } else {
-                response.status(404).end(`${id} not found`)
-            }
-        })
-        .catch(error => {
-            next(error)
-        })
+  const id = request.params.id
+  Contact.findById(request.params.id)
+    .then(res => {
+      if (res) {
+        response.json(res)
+      } else {
+        response.status(404).end(`${id} not found`)
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const  body = request.body
-    const id = request.params.id
-    const newContact = {
-        name: body.name,
-        number: body.number
-    }
+  const  body = request.body
+  const id = request.params.id
+  const newContact = {
+    name: body.name,
+    number: body.number
+  }
 
-    Contact.findByIdAndUpdate(id, newContact, { new: true, runValidators: true }).then(res => {
-        if (res) {
-           response.json(res)
-        } else {
-            response.status(404).end(`${id} not found`)
-        }
-    }).catch(error => next(error))
+  Contact.findByIdAndUpdate(id, newContact, { new: true, runValidators: true }).then(res => {
+    if (res) {
+      response.json(res)
+    } else {
+      response.status(404).end(`${id} not found`)
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', async (request, response, next) => {
-    const id = request.params.id
-    Contact.findByIdAndDelete(id).then(
-        deletion => {
-            if (deletion) {
-                response.json(`deleted ${id}`)
-            } else {
-                response.status(404).end(`${id} not found`)
-            }
-        }
-    ).catch(error => next(error))
+  const id = request.params.id
+  Contact.findByIdAndDelete(id).then(
+    deletion => {
+      if (deletion) {
+        response.json(`deleted ${id}`)
+      } else {
+        response.status(404).end(`${id} not found`)
+      }
+    }
+  ).catch(error => next(error))
 })
 
 app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`Server runs on ${PORT}`)
+  console.log(`Server runs on ${PORT}`)
 })
--- part3/phonebook/models/contact.js
+++ part3/phonebook/models/contact.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const { MongoNetworkError } = require('mongodb')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -8,41 +7,41 @@ const url = process.env.MONGODB_URI
 console.log(`connecting to ${url}`)
 
 const numericValidator = (number) => {
-    return /^\d+$/.test(number)
+  return /^\d+$/.test(number)
 }
 
 const numberValidator = (number) => {
 
-    if (number.includes('-')) {
-        const [prefix, suffix] = number.split('-');
-        console.log(prefix)
-        return (prefix.length === 2 || prefix.length === 3) && numericValidator(prefix) && numericValidator(suffix)
-    }
+  if (number.includes('-')) {
+    const [prefix, suffix] = number.split('-')
+    console.log(prefix)
+    return (prefix.length === 2 || prefix.length === 3) && numericValidator(prefix) && numericValidator(suffix)
+  }
 
-    return false
+  return false
 }
 
-const custom = [numberValidator, "Number is not in the valid format"]
+const custom = [numberValidator, 'Number is not in the valid format']
 
 mongoose.connect(url)
-    .then(result => { console.log('Connected to MongoDB') })
-    .catch (error => {
-        console.log('error connecting to MongoDB', error)
-    })
+  .then(() => { console.log('Connected to MongoDB') })
+  .catch (error => {
+    console.log('error connecting to MongoDB', error)
+  })
 
 const noteSchema = new mongoose.Schema(
-    {
-        name: {type: String, required: [true, 'name is required'], unique: true, minLength: 3},
-        number: {type: String, required: [true, 'number is required'], minLength: 8 , validate: custom},
-    }
+  {
+    name: { type: String, required: [true, 'name is required'], unique: true, minLength: 3 },
+    number: { type: String, required: [true, 'number is required'], minLength: 8 , validate: custom },
+  }
 )
 
 noteSchema.set('toJSON', {
-    transform: (document, output) => {
-        output.id = document._id.toString()
-        delete output._id
-        delete output.__v
-    }
+  transform: (document, output) => {
+    output.id = document._id.toString()
+    delete output._id
+    delete output.__v
+  }
 })
 
 module.exports = mongoose.model('Contact', noteSchema)
